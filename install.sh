1f75629f63d60505966641a95f30d00a32e55403
SidneyW <sidneywijngaarde@gmail.com>
2016-05-25 23:38:25 -0400

    Created installation script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Creates symlinks for dotfiles
+dir="$HOME/.dot_files"
+
+echo "Dot files folder: $dir"
+
+ln -s $dir/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $dir/vim/vimrc $HOME/.vimrc
+ln -s $dir/vim $HOME/.vim
+ln -s $dir/bash_profile $HOME/.bash_profile
+
+if [[ -d $HOME/bin ]]
+then
+	echo User level bin already exists
+else
+	ln -s $dir/bin ~/bin
+fi
