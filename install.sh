010024432f254130aae6549880077044f766a8f0
SidneyW <sidneywijngaarde@gmail.com>
2017-07-23 15:17:55 -0400

    removed vim

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ dir="$HOME/.dot_files"
 echo "Dot files folder: $dir"
 
 ln -s $dir/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $dir/tmux/tmux.conf.local $HOME/.tmux.conf.local
+
+ln -s $dir/nvim $HOME/.config/nvim
+ln -s $dir/bash_profile $HOME/.bash_profile
+
 ln -s $dir/vim/vimrc $HOME/.vimrc
 ln -s $dir/vim $HOME/.vim
-ln -s $dir/nvim $HOME/.config/nvim
-ln -s $dir/vim/vimrc $HOME/.config/nvim/init.vim
-ln -s $dir/bash_profile $HOME/.bash_profile
+
 ln -s $dir/gitconfig $HOME/.gitconfig
 
 if [[ -d $HOME/bin ]]
