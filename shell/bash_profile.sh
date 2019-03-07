77c69804ae6396d626b4ab1975ef9dc0740dbdbe
sidneyw <sidneywijngaarde@gmail.com>
2019-03-06 21:22:24 -0800

    Disable prompt from pyenv

--- shell/bash_profile.sh
+++ shell/bash_profile.sh
@@ -332,6 +332,7 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 	eval "$(pyenv virtualenv-init -)"
+	export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 fi
 
 # heroku autocomplete setup
