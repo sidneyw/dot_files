1d5468f99e50704a960b699e7d99b62a6a321b11
Sidney Wijngaarde <sidney@chronosphere.io>
2022-06-11 14:32:42 -0400

    track global lua

---
+++ nvim/lua/sidneyw/global.lua
@@ -0,0 +1,40 @@
+local ok, plenary_reload = pcall(require, "plenary.reload")
+if not ok then
+	reloader = require
+else
+	reloader = plenary_reload.reload_module
+end
+
+P = function(v)
+	print(vim.inspect(v))
+	return v
+end
+
+RELOAD = function(...)
+	return reloader(...)
+end
+
+R = function(name)
+	RELOAD(name)
+	return require(name)
+end
+
+function nnoremap(lhs, fn)
+	vim.keymap.set("n", lhs, fn, { noremap = true })
+end
+
+function imap(tbl)
+	vim.keymap.set("i", tbl[1], tbl[2], tbl[3])
+end
+
+function nmap(tbl)
+	vim.keymap.set("n", tbl[1], tbl[2], tbl[3])
+end
+
+-- Reload init.lua
+nmap({
+	"<leader>sv",
+	function()
+		R("sidneyw.init")
+	end,
+})
