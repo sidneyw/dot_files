79eb9e9642eaf767a261b17ddf5fa87edb00bedb
Sidney Wijngaarde <sidney@chronosphere.io>
2022-11-27 20:12:23 -0500

    use replace mode for inserting in autocomplete

--- nvim/lua/sidneyw/plugins/luasnip-cmp.lua
+++ nvim/lua/sidneyw/plugins/luasnip-cmp.lua
@@ -3,11 +3,6 @@ local cmp = require("cmp")
 local lspkind = require("lspkind")
 local luasnip = require("luasnip")
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 require("luasnip.loaders.from_snipmate").load()
 
 cmp.setup({
@@ -21,6 +16,8 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
+    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Replace },
+    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Replace },
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
