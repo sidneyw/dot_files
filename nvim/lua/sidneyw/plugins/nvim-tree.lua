d61d93673999948cb533cefcdc977b28ea961221
Sidney Wijngaarde <sidney@chronosphere.io>
2022-11-27 22:42:35 -0500

    nvim-tree respect buf cwd

--- nvim/lua/sidneyw/plugins/nvim-tree.lua
+++ nvim/lua/sidneyw/plugins/nvim-tree.lua
@@ -2,18 +2,15 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require("nvim-tree").setup({
 	sync_root_with_cwd = true,
-	-- respect_buf_cwd = false, -- default setting is false
+	respect_buf_cwd = true, -- default setting is false
+  update_cwd = false,
 	disable_netrw = false,
-	hijack_netrw = true,
-	open_on_setup = false,
 	open_on_tab = false,
 	hijack_directories = {
 		enable = true,
 		auto_open = false,
 	},
 	ignore_ft_on_setup = {},
-	hijack_cursor = false,
-	update_cwd = false,
 	diagnostics = {
 		enable = true,
 		icons = {
@@ -23,11 +20,6 @@ require("nvim-tree").setup({
 			error = "",
 		},
 	},
-	git = {
-		enable = true,
-		ignore = true,
-		timeout = 500,
-	},
 	view = {
 		width = 40,
 	},
@@ -36,7 +28,7 @@ require("nvim-tree").setup({
 
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_respect_buf_cwd = 1
+-- vim.g.nvim_tree_respect_buf_cwd = 1
 -- vim.g.nvim_tree_disable_window_picker = 1
 
 vim.cmd([[ nnoremap <C-n> :NvimTreeFindFileToggle<CR> ]])
