6aa4e4852c17d273e5f083e39e8a21ab54600608
Sidney Wijngaarde <sidney@chronosphere.io>
2023-09-27 09:52:50 -0400

    Fix copliot tab overloading

--- nvim/lua/sidneyw/plugins/luasnip-cmp.lua
+++ nvim/lua/sidneyw/plugins/luasnip-cmp.lua
@@ -8,52 +8,74 @@ require("luasnip.loaders.from_snipmate").load()
 require("luasnip.loaders.from_vscode").load()
 
 local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  unpack = unpack or table.unpack
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-k>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-j>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Replace },
     ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Replace },
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip", option = { use_show_condition = false } },
-		{ name = "path" },
-		{ name = "nvim_lua" },
-		{ name = "treesitter" },
-	}, {
-		{ name = "buffer", keyword_length = 5 },
-	}),
-	formatting = {
-		format = lspkind.cmp_format({
-			with_text = true,
-			mapping = {
-        Copilot = "",
-				buffer = "[Buf]",
-				nvim_lsp = "[LSP]",
-				nvim_lua = "[Lua]",
-				path = "[Path]",
-				ultisnips = "[Ulti]",
-			},
-		}),
-	},
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert, }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip",   option = { use_show_condition = false } },
+    { name = "path" },
+    { name = "nvim_lua" },
+    { name = "treesitter" },
+  }, {
+    { name = "buffer", keyword_length = 5 },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = true,
+      mapping = {
+        -- Copilot = "",
+        buffer = "[Buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[Lua]",
+        path = "[Path]",
+        ultisnips = "[Ulti]",
+      },
+    }),
+  },
   sorting = {
     priority_weight = 2,
     comparators = {
@@ -74,37 +96,32 @@ cmp.setup({
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype("gitcommit", {
-	sources = cmp.config.sources({
-		{ name = "cmp_git" },
-	}, {
-		{ name = "buffer" },
-	}),
+  sources = cmp.config.sources({
+    { name = "cmp_git" },
+  }, {
+    { name = "buffer" },
+  }),
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline("/", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		-- Don't start completion for simple commands like :w
-		{ name = "cmdline", keyword_length = 2 },
-	}),
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    -- Don't start completion for simple commands like :w
+    { name = "cmdline", keyword_length = 2 },
+  }),
 })
 
-vim.cmd([[
-	imap <silent><expr> <C-k> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : ''
-	" -1 for jumping backwards.
-	inoremap <silent> <C-j> <cmd>lua require'luasnip'.jump(-1)<Cr>
-
-	snoremap <silent> <C-k> <cmd>lua require('luasnip').jump(1)<Cr>
-	snoremap <silent> <C-j> <cmd>lua require('luasnip').jump(-1)<Cr>
-]])
+-- Copilot settings
+vim.api.nvim_set_keymap("i", "<C-[>", 'copilot#Previous()', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-]>", 'copilot#Next()', { silent = true, expr = true })
