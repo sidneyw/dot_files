be839caf9c0cfc9838b966e6066cf56f1b384416
Sidney Wijngaarde <sidney@chronosphere.io>
2024-06-03 15:56:25 -0400

    [nvim] Remove backup files

--- config/nvim.bak/lua/sidneyw/chronosphere.lua
+++
@@ -1,7 +0,0 @@
--- TODO: Wrap in autocmd to determine if file is nested underneath the monorepo.
-nnoremap("<leader>lb", "<cmd>Make! lint-branch<CR>")
-
-nnoremap("<leader>cl", "<cmd>!cpsha<CR>")
-
-vim.cmd([[ cnoreabbr mg Make! gen ]])
-vim.cmd([[ cnoreabbr mcl Make! droidcli ]])
