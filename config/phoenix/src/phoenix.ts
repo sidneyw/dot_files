bcf6c2a248ead23fff292612fdf53328afc4689b
Sidney Wijngaarde <sidney@chronosphere.io>
2024-07-06 17:42:43 -0400

    [phoenix] Use raycast for app switching

--- config/phoenix/src/phoenix.ts
+++ config/phoenix/src/phoenix.ts
@@ -25,25 +25,25 @@ const doubleShift: Phoenix.ModifierKey[] = double.concat(["shift"]);
 // ------------
 // Launch Apps
 // ------------
-Key.on("a", alt, () => focusOrStart("Arc"));
-Key.on("q", alt, () => focusOrStart("Alacritty"));
-Key.on("b", alt, () => focusOrStart("Brave Browser"));
+// Key.on("a", alt, () => focusOrStart("Arc"));
+// Key.on("q", alt, () => focusOrStart("Alacritty"));
+// Key.on("b", alt, () => focusOrStart("Brave Browser"));
 // Took the place of  roam and therefore has r trigger
-Key.on("r", alt, () => focusOrStart("Logseq"));
-Key.on("n", alt, () => focusOrStart("Notion"));
-Key.on("c", alt, () => focusOrStart("Notion Calendar"));
-Key.on("m", alt, () => focusOrStart("Messages"));
+// Key.on("r", alt, () => focusOrStart("Logseq"));
+// Key.on("n", alt, () => focusOrStart("Notion"));
+// Key.on("c", alt, () => focusOrStart("Notion Calendar"));
+// Key.on("m", alt, () => focusOrStart("Messages"));
 // Key.on("r", alt, () => focusOrStart("Roam Research"));
-Key.on("1", alt, () => focusOrStart("Slack"));
-Key.on("p", alt, () => focusOrStart("Spark"));
-Key.on("s", alt, () => focusOrStart("Spotify"));
-Key.on("t", alt, () => focusOrStart("TickTick"));
-Key.on("z", alt, () => focusOrStart("zoom.us"));
+// Key.on("1", alt, () => focusOrStart("Slack"));
+// Key.on("p", alt, () => focusOrStart("Spark"));
+// Key.on("s", alt, () => focusOrStart("Spotify"));
+// Key.on("t", alt, () => focusOrStart("TickTick"));
+// Key.on("z", alt, () => focusOrStart("zoom.us"));
 
 // ------------
 // Move Windows
 // ------------
-Key.on("c", doubleShift, centerTwoThirds);
+// Key.on("c", doubleShift, centerTwoThirds);
 // Key.on("left", double, leftHalf);
 // Key.on("right", double, rightHalf);
 // Key.on("left", triple, prevScreen);
@@ -52,4 +52,4 @@ Key.on("c", doubleShift, centerTwoThirds);
 // Key.on("f", double, fullScreen);
 
 // Print out the name of all running apps
-Key.on("2", alt, logAllApps);
+// Key.on("2", alt, logAllApps);
